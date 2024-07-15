b474ac946d8bf81bfcf7642e3b77f6904bf20458
Mike <mihailoarr@ya.ru>
2024-07-15 15:46:00 +0500

    1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,28 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car bmw = new Car("BMW", 5);
+      Car mercedes = new Car("Mercedes", 3);
+      Car audi = new Car("Audi", 7);
+      Car toyota = new Car("Toyota", 2);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru", bmw);
+      bmw.setUser(user1);
+
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru", mercedes);
+      mercedes.setUser(user2);
+
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru", audi);
+      audi.setUser(user3);
+
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru", toyota);
+      toyota.setUser(user4);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,6 +1,15 @@
 package hiber.model;
 
-import javax.persistence.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
@@ -19,12 +28,23 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id")
+   private Car car;
+
    public User() {}
-   
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+   }
+
+   public User(String firstName, String lastName, String email, Car car) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.car = car;
    }
 
    public Long getId() {
